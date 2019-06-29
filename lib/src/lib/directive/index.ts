0033168d1283a69d7f7ba88a4bd207aaa5bfb18c
John Carroll <john.carroll.p@gmail.com>
2019-06-28 20:14:16 -0700

    fix build errors

--- lib/src/lib/directive/index.ts
+++
@@ -1,6 +0,0 @@
-export { IsLoadingDirectiveModule } from './is-loading.directive.module';
-export { IsLoadingDirective } from './is-loading.directive';
-export {
-  SW_IS_LOADING_DIRECTIVE_CONFIG,
-  ISWIsLoadingDirectiveConfig,
-} from './is-loading.directive.config';
--- lib/src/public_api.ts
+++ lib/src/public_api.ts
@@ -2,10 +2,17 @@
  * Public API Surface of is-loading
  */
 
-export { LoadingOptions, IsLoadingService } from './lib/is-loading.service';
-export {
-  IsLoadingDirectiveModule,
-  IsLoadingDirective,
-  SW_IS_LOADING_DIRECTIVE_CONFIG,
-  ISWIsLoadingDirectiveConfig,
-} from './lib/directive';
+/**
+ * Important!
+ * The IsLoadingDirectiveModule files must be exported
+ * individually without using an `index.ts` file in the
+ * `./lib/directive` folder. Otherwise there's some sort of
+ * bug with `ng-packagr` or `angular` which prevents the
+ * `service-work-is-loading.metadata.json` from being
+ * properly built.
+ */
+
+export * from './lib/is-loading.service';
+export * from './lib/directive/is-loading.directive.module';
+export * from './lib/directive/is-loading.directive';
+export * from './lib/directive/is-loading.directive.config';
