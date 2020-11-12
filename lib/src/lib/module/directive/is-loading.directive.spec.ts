ccaef33ff2ab9bad39fa1927787989ed9086210d
John Carroll <john.carroll.p@gmail.com>
2020-11-12 01:41:55 +0000

    Use jest for testing

--- lib/src/lib/module/directive/is-loading.directive.spec.ts
+++ lib/src/lib/module/directive/is-loading.directive.spec.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, ElementRef } from "@angular/core";
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { IsLoadingDirectiveModule } from "./is-loading.directive.module";
 import { IsLoadingService } from "../../is-loading.service";
 import { IsLoadingDirective } from "./is-loading.directive";
